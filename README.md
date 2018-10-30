cb605c19365ff69796c3412887fb5ada7e485b33
Andrews Ang <andrews.ang@redooor.com>
2018-10-30 16:50:48 +0800

    Fixed OAuth to 2-legged instead. Changed port to 3000. Change route naming convention. Improved Readme.md to align with FPD.

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 [![Node.js](https://img.shields.io/badge/Node.js-8.11.1-blue.svg)](https://nodejs.org/)
 [![npm](https://img.shields.io/badge/npm-6.1.0-blue.svg)](https://www.npmjs.com/)
+![Platforms](https://img.shields.io/badge/platform-windows%20%7C%20osx%20%7C%20linux-lightgray.svg)
 [![License](http://img.shields.io/:license-mit-blue.svg)](http://opensource.org/licenses/MIT)
 
 [![oAuth2](https://img.shields.io/badge/oAuth2-v1-green.svg)](http://developer.autodesk.com/)
 [![Reality-Capture](https://img.shields.io/badge/Reality%20Capture-v1-green.svg)](http://developer.autodesk.com/)
 
-## Description
+# Description
+This sample is part of the [Photo to 3D Walkthrough](https://forge.autodesk.com/developer/learn/recap-app).
+
 This sample illustrates the Reality Capture API's photogrammetry workflow:
 - Create a photoscene
 - Add images to the photoscene
@@ -16,32 +19,63 @@ This sample illustrates the Reality Capture API's photogrammetry workflow:
 - Retrieve the generated 3D file
 - Delete the photoscene
 
-## Setup & Run
-1. Setup
-   - Install `npm` (if you haven't already).
-   - Install `npm` packages using the command `npm install`.
-     - Make sure that the current directory contains `package.json`.
-   - Ensure that in your hosts file you have mapped:
-     - `127.0.0.1 localhost`
-   - Replace the `FORGE_CLIENT_ID` and `FORGE_CLIENT_SECRET` variable values in `start.js` with your Forge credentials.
+## Thumbnail
+![thumbnail](/thumbnail.png)
 
-2. Execute `node start.js`
-   - The node server will listen on on port `5000`.
+# Setup
 
-3. Open a browser and navigate to `http://localhost:5000/`.
+## Prerequisites
 
-4. Click on the `Authorize me` link and login to your Autodesk account.
-   - In your console, you should see `Server listening on port 5000`.
-   - Once authenticated, click through the links on the browser to:
-      - Add a photoscene
-      - Upload files to photoscene
-      - Begin processing photoscene
-      - Check progress of photoscene (you will see the progress in percentage, keep refreshing until it's DONE)
-      - Delete photoscene
+1. A Forge account: [Getting Started with Forge](https://forge.autodesk.com/developer/getting-started)
+2. A text editor (We recommend http://brackets.io/ or https://atom.io/, it's up to you.)
+3. A basic knowledge of :
+    - HTML and CSS
+    - JavaScript ES6
+    - Command-line programs
+      - Node.js Command Line (for Windows users)
+      - Terminal (for Mac/Linux/Unix users)
+
+## Running locally
+
+Install [NodeJS](https://nodejs.org/) (version 8 or newer).
+
+Clone this project or download it. It's recommended to install [GitHub desktop](https://desktop.github.com/).
+
+To run it, install the required packages, set the enviroment variables with your client ID & secret and finally start it. Via command line, navigate to the folder where this repository was cloned and use the following:
+
+Mac OSX/Linux (Terminal)
+
+```bash
+npm install
+export FORGE_CLIENT_ID=<<YOUR CLIENT ID FROM DEVELOPER PORTAL>>
+export FORGE_CLIENT_SECRET=<<YOUR CLIENT SECRET>>
+npm start
+```
+
+Windows (use Node.js command line from Start menu)
+
+```bash
+npm install
+set FORGE_CLIENT_ID=<<YOUR CLIENT ID FROM DEVELOPER PORTAL>>
+set FORGE_CLIENT_SECRET=<<YOUR CLIENT SECRET>>
+npm start
+```
+
+Open a browser and navigate to http://localhost:3000.
+
+Click on the `Authorize me` link and then click through the links on the browser to:
+  - Add a photoscene
+  - Upload files to photoscene
+  - Begin processing photoscene
+  - Check progress of photoscene (you will see the progress in percentage, keep refreshing until it's DONE)
+  - Delete photoscene
 
 *NOTE:* _It may take several minutes to complete processing a photoscene._
 
 _For a small project (< 20 photos), it shouldn’t take more than 15 minutes to complete, otherwise it takes about 6 hours for 500 photos, and less than 16 hours for 1000 photos. The percentage progress is calibrated for larger projects (that take a few hours to process)._
 
-### Thumbnail
-![thumbnail](/thumbnail.png)
+# License
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT). Please see the [LICENSE](LICENSE) file for full details.
+
+# Support
+forge.help@autodesk.com
--- start.js
+++ start.js
@@ -29,8 +29,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/www'));
 
-// This is for web server to start listening to port 5000
-app.set('port', 5000);
+// This is for web server to start listening to port 3000
+app.set('port', 3000);
 var server = app.listen(app.get('port'), function () {
     console.log('Server listening on port ' + server.address().port);
 });
@@ -40,45 +40,32 @@ var server = app.listen(app.get('port'), function () {
 // Initialize the 2-legged OAuth2 client, set specific scopes and
 // set the token to auto refresh
 //-------------------------------------------------------------------
-var FORGE_CLIENT_ID = '<REPLACE_WITH_FORGE_CLIENT_ID>';
-var FORGE_CLIENT_SECRET = '<REPLACE_WITH_FORGE_CLIENT_SECRET>';
-var FORGE_CALLBACK_URL = 'http://localhost:5000/callback';
+var FORGE_CLIENT_ID = process.env.FORGE_CLIENT_ID;
+var FORGE_CLIENT_SECRET = process.env.FORGE_CLIENT_SECRET;
+var access_token = '';
 var scopes = 'data:read data:write';
 const querystring = require('querystring');
 
-// Route /auth
-// Redirect to Autodesk sign in page for end user to login
-app.get('/auth', function (req, res) {
-    var redirect_uri = 'https://developer.api.autodesk.com/authentication/v1/authorize?'
-    + 'response_type=code'
-    + '&client_id=' + FORGE_CLIENT_ID
-    + '&redirect_uri=' + encodeURIComponent(FORGE_CALLBACK_URL)
-    + '&scope=' + encodeURIComponent(scopes);
-    res.redirect(redirect_uri);
-});
-
-// Route /callback
-// Get Authorization code from Autodesk signin
-app.get('/callback', function (req, res) {
+// // Route /api/forge/oauth
+app.get('/api/forge/oauth', function (req, res) {
     Axios({
         method: 'POST',
-        url: 'https://developer.api.autodesk.com/authentication/v1/gettoken',
+        url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
         headers: {
             'content-type': 'application/x-www-form-urlencoded',
         },
         data: querystring.stringify({
             client_id: FORGE_CLIENT_ID,
             client_secret: FORGE_CLIENT_SECRET,
-            grant_type: 'authorization_code',
-            code: req.query.code,
-            redirect_uri: FORGE_CALLBACK_URL
+            grant_type: 'client_credentials',
+            scope: scopes
         })
     })
         .then(function (response) {
             // Success
             access_token = response.data.access_token;
             console.log(response);
-            res.send('<p>Authentication success!</p><a href="/photoscene/add?token=' + access_token + '">Add a photoscene</a>');
+            res.send('<p>Authentication success!</p><a href="/api/forge/recap/photoscene/add">Add a photoscene</a>');
         })
         .catch(function (error) {
             // Failed
@@ -87,10 +74,9 @@ app.get('/callback', function (req, res) {
         });
 });
 
-// Route /photoscene/add
+// Route /api/forge/recap/photoscene/add
 // Creates and initializes a photoscene for reconstruction.
-app.get('/photoscene/add', function (req, res) {
-    var access_token = req.query.token;
+app.get('/api/forge/recap/photoscene/add', function (req, res) {
     Axios({
         method: 'POST',
         url: 'https://developer.api.autodesk.com/photo-to-3d/v1/photoscene',
@@ -110,7 +96,7 @@ app.get('/photoscene/add', function (req, res) {
                 res.send(response.data.Error.msg);
             }
             var photosceneId = response.data.Photoscene.photosceneid;
-            var nextLink = '/photoscene/upload?token=' + access_token + '&photosceneid=' + photosceneId;
+            var nextLink = '/api/forge/recap/photoscene/upload?photosceneid=' + photosceneId;
             res.send('<p>Photoscene added!</p><a href="' + nextLink + '">Upload files to photoscene</a>');
         })
         .catch(function (error) {
@@ -120,10 +106,9 @@ app.get('/photoscene/add', function (req, res) {
         });
 });
 
-// Route /photoscene/upload
+// Route /api/forge/recap/photoscene/upload
 // Adds one or more files to a photoscene.
-app.get('/photoscene/upload', function (req, res) {
-    var access_token = req.query.token;
+app.get('/api/forge/recap/photoscene/upload', function (req, res) {
     var photosceneId = req.query.photosceneid;
     Axios({
         method: 'POST',
@@ -151,7 +136,7 @@ app.get('/photoscene/upload', function (req, res) {
                 res.send(response.data.Error.msg);
             }
             console.log(JSON.stringify(response.data.Files));
-            var nextLink = '/photoscene/process?token=' + access_token + '&photosceneid=' + photosceneId;
+            var nextLink = '/api/forge/recap/photoscene/process?photosceneid=' + photosceneId;
             res.send('<p>Files added to photoscene!</p><a href="' + nextLink + '">Begin processing photoscene</a>');
         })
         .catch(function (error) {
@@ -161,10 +146,9 @@ app.get('/photoscene/upload', function (req, res) {
         });
 });
 
-// Route /photoscene/process
+// Route /api/forge/recap/photoscene/process
 // Starts photoscene processing.
-app.get('/photoscene/process', function (req, res) {
-    var access_token = req.query.token;
+app.get('/api/forge/recap/photoscene/process', function (req, res) {
     var photosceneId = req.query.photosceneid;
     Axios({
         method: 'POST',
@@ -180,7 +164,7 @@ app.get('/photoscene/process', function (req, res) {
             if (response.data.Error) {
                 res.send(response.data.Error.msg);
             }
-            var nextLink = '/photoscene/checkprogress?token=' + access_token + '&photosceneid=' + photosceneId;
+            var nextLink = '/api/forge/recap/photoscene/checkprogress?photosceneid=' + photosceneId;
             res.send('<p>Photoscene is being processed!</p><a href="' + nextLink + '">Check progress of photoscene</a>');
         })
         .catch(function (error) {
@@ -190,10 +174,9 @@ app.get('/photoscene/process', function (req, res) {
         });
 });
 
-// Route /photoscene/checkprogress
+// Route /api/forge/recap/photoscene/checkprogress
 // Returns the processing progress and status of a photoscene.
-app.get('/photoscene/checkprogress', function (req, res) {
-    var access_token = req.query.token;
+app.get('/api/forge/recap/photoscene/checkprogress', function (req, res) {
     var photosceneId = req.query.photosceneid;
     Axios({
         method: 'GET',
@@ -210,11 +193,11 @@ app.get('/photoscene/checkprogress', function (req, res) {
                 res.send(response.data.Error.msg);
             }
             if (response.data.Photoscene && response.data.Photoscene.progressmsg == 'DONE') {
-                var nextLink = '/photoscene/result?token=' + access_token + '&photosceneid=' + photosceneId;
+                var nextLink = '/api/forge/recap/photoscene/result?photosceneid=' + photosceneId;
                 res.send('<p>Photoscene process is complete!</p><a href="' + nextLink + '">View result of photoscene</a>');
             } else {
-                var nextLink = '/photoscene/delete?token=' + access_token + '&photosceneid=' + photosceneId;
-                res.send('<p>Photoscene is not ready, this may take a while. Try refreshing this page. Progress: ' + response.data.Photoscene.progress + '%...</p>');
+                var nextLink = '/api/forge/recap/photoscene/delete?photosceneid=' + photosceneId;
+                res.send('<p>Photoscene is not ready, this may take a while. Try refreshing <a href="/api/forge/recap/photoscene/checkprogress?photosceneid=' +  photosceneId + '">this page</a>. Progress: ' + response.data.Photoscene.progress + '%...</p>');
             }
             
         })
@@ -225,10 +208,9 @@ app.get('/photoscene/checkprogress', function (req, res) {
         });
 });
 
-// Route /photoscene/result
+// Route /api/forge/recap/photoscene/result
 // Returns a time-limited HTTPS link to an output file of the specified format.
-app.get('/photoscene/result', function (req, res) {
-    var access_token = req.query.token;
+app.get('/api/forge/recap/photoscene/result', function (req, res) {
     var photosceneId = req.query.photosceneid;
     Axios({
         method: 'GET',
@@ -245,11 +227,11 @@ app.get('/photoscene/result', function (req, res) {
                 res.send(response.data.Error.msg);
             }
             if (response.data.Photoscene && response.data.Photoscene.progressmsg == 'DONE') {
-                var nextLink = '/photoscene/delete?token=' + access_token + '&photosceneid=' + photosceneId;
+                var nextLink = '/api/forge/recap/photoscene/delete?photosceneid=' + photosceneId;
                 res.send('<p>Success! This is the scene link:</p><p>' + response.data.Photoscene.scenelink + '</p>'
                     + 'Would you like to <a href="' + nextLink + '">delete photoscene</a>?');
             } else {
-                res.send('Photoscene is not ready. Try refreshing this page. Progress: ' + response.data.Photoscene.progress + '%...');
+                res.send('Photoscene is not ready. Try refreshing <a href="/api/forge/recap/photoscene/checkprogress?photosceneid=' +  photosceneId + '">this page</a>. Progress: ' + response.data.Photoscene.progress + '%...');
             }
             
         })
@@ -260,10 +242,9 @@ app.get('/photoscene/result', function (req, res) {
         });
 });
 
-// Route /photoscene/delete
+// Route /api/forge/recap/photoscene/delete
 // Deletes a photoscene and its associated assets (images, output files, ...).
-app.get('/photoscene/delete', function (req, res) {
-    var access_token = req.query.token;
+app.get('/api/forge/recap/photoscene/delete', function (req, res) {
     var photosceneId = req.query.photosceneid;
     Axios({
         method: 'DELETE',
